43ce68913892dc425f5a7ae9f9791dfde39a4c99
emobrya <emobrya@gmail.com>
2025-03-10 17:28:31 +0300

    feat: Route - add app routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ProductController::class, 'index'])->name('home');
+Route::post('/submit', [ProductController::class, 'submit'])->name('submit');
+Route::post('/edit/{id}', [ProductController::class, 'edit'])->name('edit');
